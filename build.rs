10f1129efaaaff787a206e3dc6dd38171ae2b7e4
Skyler Lipthay <sl@skylerlipthay.com>
2018-06-18 19:06:35 -0700

    Add `prevent-tracebacks` feature

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
 extern crate cc;
 
+#[cfg(feature = "prevent-tracebacks")]
+const PREVENT_TRACEBACKS: bool = true;
+#[cfg(not(feature = "prevent-tracebacks"))]
+const PREVENT_TRACEBACKS: bool = false;
+
 fn main() {
-    cc::Build::new()
-        .include("duktape")
+    let mut builder = cc::Build::new();
+
+    builder.include("duktape")
         .flag("-std=c99")
         .file("duktape/duktape.c")
-        .file("duktape/wrapper.c")
-        .compile("libduktape.a");
+        .file("duktape/wrapper.c");
+
+    if PREVENT_TRACEBACKS {
+        builder.define("RUST_DUK_PREVENT_TRACEBACKS", None);
+    }
+
+    builder.compile("libduktape.a");
 }
